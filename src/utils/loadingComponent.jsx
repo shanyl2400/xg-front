463b9ad290200365211c70e797ce3773150c929a
white.shan <white.shan@kidsloop.cn>
2021-01-15 22:26:37 +0800

    update loading

--- src/utils/loadingComponent.jsx
+++ src/utils/loadingComponent.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import Loadable from 'react-loadable';
+import { Spin } from 'antd';
 
 //通用的过场组件
-const loadingComponent =()=>{
+const loadingComponent = () => {
     return (
-        <div>loading</div>
+        <div>
+            <Spin style={{
+                position: "absolute",
+                top: 50,
+                right: 0,
+                left: 0,
+                bottom: 0,
+                margin: "auto"
+            }}
+                tip="加载中..." />
+        </div>
     )
 }
 
 //过场组件默认采用通用的，若传入了loading，则采用传入的过场组件
-export default (loader,loading = loadingComponent)=>{
+export default (loader, loading = loadingComponent) => {
     return Loadable({
         loader,
         loading
