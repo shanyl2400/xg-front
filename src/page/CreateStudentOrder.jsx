6f287018fce68d00ad931b1e419075855fc6a4ef
white.shan <white.shan@kidsloop.cn>
2021-01-17 15:09:11 +0800

    update create order page

--- src/page/CreateStudentOrder.jsx
+++ src/page/CreateStudentOrder.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card, Breadcrumb, message, Row, Col, Select, Typography, Table, Pagination } from 'antd';
+import { Button, Card, Breadcrumb, message, Row, Col, Select, Typography, Table, Pagination, Descriptions } from 'antd';
 import { useParams, useHistory } from "react-router-dom";
 import SubOrgFilter from '../component/SubOrgFilter';
 import CreateOrderModal from '../component/CreateOrderModal';
@@ -209,7 +209,24 @@ function CreateStudentOrder(props) {
                 <Breadcrumb.Item>学员管理</Breadcrumb.Item>
                 <Breadcrumb.Item>创建订单</Breadcrumb.Item>
             </Breadcrumb>
-            <Card style={{ width: "100%", margin: "20px 5px" }}>
+            <Descriptions title="学员信息" bordered style={{ marginBottom: 40, marginTop: 20 }}>
+                <Descriptions.Item label="姓名">{student.name}</Descriptions.Item>
+                <Descriptions.Item label="性别">{student.gender ? "男" : "女"}</Descriptions.Item>
+                <Descriptions.Item label="邮箱">{student.email}</Descriptions.Item>
+                <Descriptions.Item label="手机号：">{student.telephone}</Descriptions.Item>
+                <Descriptions.Item label="代理人">{student.authorName}</Descriptions.Item>
+                <Descriptions.Item label="居住地址" >{parseAddress(student.address)}{student.address_ext}</Descriptions.Item>
+                <Descriptions.Item label="报名意向">
+                    <div style={{ margin: "0px 0px" }}>
+                        {student.intent_subject != undefined && student.intent_subject.map(item => (
+                            <div key={item}>{item}</div>
+                        ))}
+                    </div>
+                </Descriptions.Item>
+                <Descriptions.Item label="订单来源" span={3}>{student.order_source_name}</Descriptions.Item>
+                <Descriptions.Item label="备注">{student.note}</Descriptions.Item>
+            </Descriptions>
+            {/* <Card style={{ width: "100%", margin: "20px 5px" }}>
                 <Row gutter={[16, 16]}>
                     <Col span={12}>姓名：{student.name}</Col>
                     <Col span={12}>性别：{student.gender ? "男" : "女"}</Col>
@@ -235,7 +252,7 @@ function CreateStudentOrder(props) {
                 <Row gutter={[16, 16]}>
                     <Col span={24}>备注：{student.note}</Col>
                 </Row>
-            </Card>
+            </Card> */}
 
             {student.orders != undefined && student.orders.length > 0 ? <Title level={4}>已推荐机构</Title> : ""}
             {student.orders != undefined && student.orders.map((order) => (
@@ -263,7 +280,7 @@ function CreateStudentOrder(props) {
 
             <Row gutter={[16, 16]} style={{ marginTop: "30px" }}>
                 {/* <Col offset={18} span={2}><Button type="primary" onClick={() => handleCreateOrder()}>派单</Button></Col> */}
-                <Col offset={22} span={2}><Button onClick={() => history.goBack()}>返回</Button></Col>
+                <Col offset={23} span={2}><Button onClick={() => history.goBack()}>返回</Button></Col>
             </Row>
             <CreateOrderModal studentId={id} visible={createOrderModalVisible} org={selectedOrg} closeModel={closeCreateOrderModal} />
         </div>
